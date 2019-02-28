57cff9479b4ca8549f2c55f48f7450f7ee60419d
InvaderCM <1292689963@qq.com>
2019-02-28 11:05:18 +0800

    helloworld project first commit

---
+++ src/main/java/com/soft1721/spring/hello/studentAPP.java
@@ -0,0 +1,12 @@
+package com.soft1721.spring.hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class studentAPP {
+    public static void main(String[] args) {
+            ApplicationContext context = new ClassPathXmlApplicationContext("/beans.xml");
+            student student = (student) context.getBean("student");
+            System.out.println(student.getName()+student.getAge()+student.getPhone());
+        }
+}
